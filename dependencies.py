0b8c24a5328745577905f54e279bdf60ef7e7a30
shaileshpandit141 <shaileshpandit141@gmail.com>
2024-03-24 07:49:58 +0530

    Add dependencies.py file and configure it.

---
+++ dependencies.py
@@ -0,0 +1,15 @@
+from os import system
+
+
+def run_command(command_name: str) -> None:
+    system(command_name)
+
+
+# Installing requirements.txt using pip.
+run_command("pip install -r requirements.txt")
+
+# Installing live-server using pip.
+run_command("pip install live-server")
+
+# Installing git using pip.
+run_command("pip install python-git")
